be9674135e3f90a2f309cd90a4d37bef5d4f4b94
Zef Houssney <zef@madebykiwi.com>
2014-10-07 16:11:52 -0600

    Initial Commit

---
+++ GradientAttributes/GradientAttributes.h
@@ -0,0 +1,19 @@
+//
+//  GradientAttributes.h
+//  GradientAttributes
+//
+//  Created by Zef Houssney on 10/7/14.
+//  Copyright (c) 2014 Made by Kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientAttributes.
+FOUNDATION_EXPORT double GradientAttributesVersionNumber;
+
+//! Project version string for GradientAttributes.
+FOUNDATION_EXPORT const unsigned char GradientAttributesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientAttributes/PublicHeader.h>
+
+
